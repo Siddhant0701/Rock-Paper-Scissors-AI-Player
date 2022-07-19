cd5963642ca7b5443e500257a49ba9d65a937a68
Siddhant0701 <mahajansiddhant07@gmail.com>
2022-07-19 18:08:48 -0400

    ref: Data files moved and added

---
+++ data/data_processing.py
@@ -0,0 +1,15 @@
+s = "twwtlwtwtwwllwlllwlwwtlttllltwlwwwtwllwwllllwlwtllwwlwwlllwtlwtwtwwttttwlwtttwlwlltwttwlwtwtltlwllttwlwtlltlllltwwlwwwlwlwwllwtltwtllwlttwltwwwwltlllttttllttttltlwwwllltlwtwlwwtwlwttlwwwwlwtlwwtltwt"
+
+wins = 0
+losses = 0
+
+for c in s:
+    if c == 'w':
+        wins += 1
+    elif c == 'l':
+        losses += 1
+    else:
+        pass
+
+print(f"Wins: {wins/len(s)}")
+print(f"Losses: {losses/len(s)}")    
